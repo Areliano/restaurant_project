cc4d18452b3579e31dbab5ac045550e1db8d6144
Fatma <fatmahussein355@gmail.com>
2025-06-15 13:20:25 +0300

    Add build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
-# Set Python version explicitly
-pyenv install -s 3.10.12
-pyenv global 3.10.12
 
-# Create a virtual environment using correct Python version
-python -m venv venv
-source venv/bin/activate
-
-# Install requirements
 pip install --upgrade pip
 pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
+
